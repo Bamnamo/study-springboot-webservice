6a90ec9a277f952a2423c8d3ed1257c62df40c0a
박성범 <>
2020-06-03 16:34:22 +0900

    05

---
+++ src/main/java/com/springboot/study/test/web/dto/PostsResponseDto.java
@@ -0,0 +1,18 @@
+package com.springboot.study.test.web.dto;
+
+import com.springboot.study.test.domian.posts.Posts;
+
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
