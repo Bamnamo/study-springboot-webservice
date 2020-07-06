319bea00ab0cc0a8ffcf8671dfbf899c62a57d83
박성범 <>
2020-07-06 16:17:02 +0900

    오류 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,8 +13,7 @@ CURRENT_PID=$(pgrep -fl study-springboot-webservice | grep jar | awk '{print $1}
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
-if [ -z "$CURRENT_PID" ]
-then
+if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
@@ -35,6 +34,6 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
--Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml \
+-Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
 -Dspring.profiles.active=real \
 $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
